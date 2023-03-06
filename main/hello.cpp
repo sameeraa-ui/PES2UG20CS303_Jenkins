2792a4570cd360b53598bb454a86f6d58ba57251
sameeraa-ui <78339968+sameeraa-ui@users.noreply.github.com>
2023-03-06 10:36:03 +0530

    update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfullyyyy built and run\n";
+  cout << "I have successfullyyyyy built and run\n";
   return 0;
 }
